bd151afd760d860efa692fe3c56416733361e6dc
EmersonVitorSouza <evsemerson1@gmail.com>
2025-10-30 17:22:08 -0300

    Modificação de users para usuarios, versão 0.2.

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def register():
         with get_conn() as conn:
             with conn.cursor() as cur:
                 try:
-                    cur.execute("INSERT INTO users (username,password_hash) VALUES (%s,%s) RETURNING id", (username,pw_hash))
+                    cur.execute("INSERT INTO usuarios (username,password_hash) VALUES (%s,%s) RETURNING id", (username,pw_hash))
                     conn.commit()
                     flash("Cadastro realizado. Faça login.")
                     return redirect(url_for("login"))
@@ -70,7 +70,7 @@ def login():
         password = request.form["password"]
         with get_conn() as conn:
             with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT id, password_hash FROM users WHERE username=%s", (username,))
+                cur.execute("SELECT id, password_hash FROM usuarios WHERE username=%s", (username,))
                 user = cur.fetchone()
                 if user and check_password_hash(user["password_hash"], password):
                     session["user_id"] = user["id"]
